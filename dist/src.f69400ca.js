bd2e7b4a505660316e3f6fada3072f1358af237b
Brianna Workman <brianna.workman@hotmail.com>
2020-10-04 21:43:32 -0400

    cache

--- dist/src.f69400ca.js
+++ dist/src.f69400ca.js
@@ -37779,7 +37779,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56657" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58176" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
